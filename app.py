fb09c716ba166f4375cb369a1782a52368778562
Pranay Lobo <42401887+pranaylobo@users.noreply.github.com>
2021-04-21 20:25:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import jsonify
 from flask import Flask
 from flask import request
 from flask_restful import reqparse, abort, Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
