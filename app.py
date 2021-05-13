828b02ff365a313a396b7a502fc6b84a480150ca
Pranay Lobo <42401887+pranaylobo@users.noreply.github.com>
2021-05-13 18:25:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,80 @@ from flask import Flask
 from flask import request
 from flask_restful import reqparse, abort, Api, Resource
 from flask_cors import CORS
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import os
 
 app = Flask(__name__)
 CORS(app)
-parser = reqparse.RequestParser()
-parser.add_argument('place', type=str)
 
-@app.route('/')
-def hello():
+@app.route('/reminder',methods=['GET'])
+def main():
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("/app/.apt/usr/bin/google-chrome")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("/app/.chromedriver/bin/chromedriver"), chrome_options=chrome_options)
+
+
+    # driver = webdriver.Chrome('chromedriver.exe')
+    driver.get("https://www.cowin.gov.in/home")
+    
+    print(driver.title)
+    search_bar = driver.find_element_by_id("mat-input-0")
+    # search_bar.clear()
+
+    
+  
+    search_bar.send_keys("400068")
+    search_bar.send_keys(Keys.RETURN)
+
+
+    # slots-box
+    # searchResults = driver.find_elements_by_class_name('vaccine-box1')
+    raw = driver.find_elements(By.XPATH, "/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[7]")
+
+
+    arr=['DAHISAR COVID JUMBO -10 Dahisar, Mumbai, Maharashtra, 400068 NA NA Booked COVISHIELD Age 45+ NA NA NA NA DAHISAR COVID JUMBO -8 Dahisar, Mumbai, Maharashtra, 400068 NA NA Booked COVISHIELD Age 45+ NA NA NA NA DAHISAR COVID JUMBO 1 Dahisar, Mumbai, Maharashtra, 400068 Booked COVISHIELD Age 45+ NA NA NA NA NA NA DAHISAR COVID JUMBO 6 Dahisar, Mumbai, Maharashtra, 400068 NA NA Booked COVISHIELD Age 45+ NA NA NA NA MEENATAI THAKRE MKT WARD NO. 4 Meenatai Thakre Market Sant Ghadge Maharaj Marg Rawalpada Dahisar East Mumbai, Mumbai, Maharashtra, 400068 Booked COVISHIELD Age 45+ NA NA NA NA NA NA SAMAJ KALYAN HALL WARD NO. 2 Samaj Kalyan Hall Near DSF Ground C. S. Main Road Dahisar East Mumbai, Mumbai, Maharashtra, 400068 Booked COVISHIELD Age 45+ NA NA NA NA NA NA']
+    
+    arr2=[]
+    arr3=[]
+    for x in raw:
+        arr2.append(x.text)
+        arr3 = arr2[0].replace("\n"," ")
+
+    print(len(arr3))
+
+    # if len(arr) is not "":
+
+   
+    
+    if len(arr3) >0:
+        if arr[0] == arr3:
+            print("No Vaccine")
+            
+
+            driver.close()
+            time.sleep(300)
+
+            main()
+
+            
+        else:
+            print("Vaccine")
+            x = requests.get('https://emailserver12.herokuapp.com/email')
+            
+
+            print(x.text)
+
+    else:
+        main()
+
+    
 
     
     return jsonify("Success")
@@ -20,42 +86,6 @@ def hello():
 
 
 
-@app.route('/get_route',methods=['POST'])
-def get_route():
-    args = parser.parse_args()
-    un = str(args['place'])
-  
-    print("hello",un)
-    Tolls_Key = 'm34GG8f7Njnrr9TmFLnB3fb4MRHhRj8J'
-    Tolls_URL = 'https://dev.tollguru.com/v1/calc/here'
-    headers = {
-            'Content-type': 'application/json',
-            'x-api-key': Tolls_Key
-          }
-    params = {
-            'from':
-               {
-                   'address': 'Borivali,Mumbai'
-               },
-           'to':
-               {'address':
-                    un
-                },
-            'vehicleType': '2AxlesAuto'
-        }
-    response = requests.post(Tolls_URL, json=params, headers=headers)
-
-
-    a = response.json()
-
-
-    arr12=[]
-    for i in range(0,len(a["routes"])):
-        dict1={}
-        dict1.update({"route":i,"Duration":a["routes"][i]["summary"]['duration']['text'],"Distance":a["routes"][i]["summary"]['distance']['metric'],"polyline":polyline.decode(a["routes"][i]["polyline"]),"cost":a["routes"][i]["costs"]['cash']})
-        arr12.append(dict1)
-
-    return jsonify(value=arr12)
 
 if __name__ == '__main__':
-   app.run(threaded=True, port=5000)
+   app.run()
